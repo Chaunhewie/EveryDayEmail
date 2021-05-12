44c29c88a5867f4ba5d462c168c4951296417d20
Chaunhewie <ChaunhewieTian@163.com>
2021-05-12 14:27:10 +0800

    fix aqi info in email
    Signed-off-by: Chaunhewie <ChaunhewieTian@163.com>

--- TXAPI.py
+++ TXAPI.py
@@ -9,30 +9,50 @@ class TXAPI:
                      "feng": 1, "yun": 2, "yu": 3, "xue": 4, "shuang": 5, "lu": 6, "wu": 7, "lei": 8, "qing": 9, "yin": 10}
     urls = {"zaoan": 'http://api.tianapi.com/txapi/zaoan/index?key={0}',
             "tianqi": 'http://api.tianapi.com/txapi/tianqi/index?key={0}&city={1}',
+            "aqi": 'http://api.tianapi.com/txapi/aqi/index?key={0}&area={1}',
             "wanan": 'http://api.tianapi.com/txapi/wanan/index?key={0}',
             "qinghua": 'http://api.tianapi.com/txapi/saylove/index?key={0}',
             "theone": 'http://api.tianapi.com/txapi/one/index?key={0}&date={1}'}
+    cache_dir = "./cache/"
 
     def __init__(self, tx_api_key):
         self.tx_api_key = tx_api_key
 
+        # make cache dirs
+        for f in self.urls.keys():
+            if not os.path.exists(os.path.join(self.cache_dir, f)):
+                os.mkdir(os.path.join(self.cache_dir, f))
+
     def get_channel_msg(self, channel, date_str, city_name):
+        urls = []
+        cache_folders = []
         if channel == "tianqi":
-            url = self.urls[channel].format(self.tx_api_key, city_name)
+            urls.append(self.urls[channel].format(self.tx_api_key, city_name))  # 天气API
+            cache_folders.append(channel)
+            urls.append(self.urls["aqi"].format(self.tx_api_key, city_name))  # 空气质量API
+            cache_folders.append("aqi")
         elif channel == "theone":
             days_before = datetime.today() + timedelta(-3)
             the_one_date_str = days_before.strftime('%Y-%m-%d')
-            url = self.urls[channel].format(self.tx_api_key, the_one_date_str)
+            urls.append(self.urls[channel].format(self.tx_api_key, the_one_date_str))
+            cache_folders.append(channel)
         else:
-            url = self.urls[channel].format(self.tx_api_key)
-        content = self.get_url_info(url, "./cache/" + channel + "/" + date_str + ".txt")
+            urls.append(self.urls[channel].format(self.tx_api_key))
+            cache_folders.append(channel)
+        content = {}
+        for i, url in enumerate(urls):
+            c = self.get_url_info(url, os.path.join(self.cache_dir, cache_folders[i], date_str + ".txt"))
+            c = c.get('newslist', [{}])[0]
+            for k, v in c.items():
+                if k not in content:
+                    content[k] = v
         # return self.execute_contents(channel, content)
         return self.execute_contents_for_html(channel, content, date_str)
 
     def get_the_one_img(self, date_str, channel="theone"):
         print("*" * 10 + "getting the one img" + "*" * 10)
         url = self.urls[channel].format(self.tx_api_key, date_str)
-        content = self.get_url_info(url, "./cache/" + channel + "/" + date_str + ".txt")
+        content = self.get_url_info(url, os.path.join(self.cache_dir, channel, date_str + ".txt"))
         return content['newslist'][0]["imgurl"]
 
     def get_url_info(self, url, file_path=""):
@@ -70,14 +90,14 @@ class TXAPI:
         :param content: url返回内容
         :return: url返回内容的处理结果
         '''
-        c = content.get('newslist', [{}])[0]
+        c = content
         if channel == "tianqi":
             msg = f"***天气预报来袭~~~\n" \
                   f"***{c.get('date', date_str)} {c.get('week', '')}\n" \
                   f"***今日{c.get('weather', '-')}\n" \
                   f"***气温{c.get('lowest', '-℃')}/{c.get('highest', '-℃')}，当前气温{c.get('real', '-℃')}\n" \
-                  f"***风力{c.get('windspeed', '-')}\n" \
-                  f"***空气质量 {c.get('air_level', '-')}\n"
+                  f"***风速{c.get('windspeed', '-')}({c.get('windsc', '-')})\n" \
+                  f"***PM2.5 {c.get('pm2_5', '-')}({c.get('quality', '-')})\n"
         elif channel == "zaoan":
             if "早安" in c.get("content", ''):
                 msg = c.get("content", '') + "\n"
@@ -103,14 +123,14 @@ class TXAPI:
         :param content: url返回内容
         :return: url返回内容的处理结果
         '''
-        c = content['newslist'][0]
+        c = content
         if channel == "tianqi":
             msg = f"<p>***天气预报来袭~~~<br>" \
                   f"***{c.get('date', date_str)} {c.get('week', '')}<br>" \
                   f"***今日{c.get('weather', '-')}<br>" \
                   f"***气温{c.get('lowest', '-℃')}/{c.get('highest', '-℃')}，当前气温{c.get('real', '-℃')}<br>" \
-                  f"***风力{c.get('windspeed', '-')}<br>" \
-                  f"***空气质量 {c.get('air_level', '-')}</p>\n"
+                  f"***风速: {c.get('windspeed', '-')}({c.get('windsc', '-')})<br>" \
+                  f"***PM2.5: {c.get('pm2_5', '-')}({c.get('quality', '-')})</p>\n"
         elif channel == "zaoan":
             if "早安" in c.get("content", ''):
                 msg = "<p>" + c.get("content", '') + "</p>\n"
